2f4d6846d54a0dcefa7836d50ae4ecc25dc0ce89
zaminalili <zaminalili29@gmail.com>
2024-11-04 12:15:14 +0400

    Add entity models for user

---
+++ SkyStorage/SkyStorage.Domain/Entities/Role.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SkyStorage.Domain.Entities;
+
+public class Role: IdentityRole<Guid>
+{
+}
---
+++ SkyStorage/SkyStorage.Domain/Entities/UserRole.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SkyStorage.Domain.Entities;
+
+public class UserRole: IdentityUserRole<Guid>
+{
+}
