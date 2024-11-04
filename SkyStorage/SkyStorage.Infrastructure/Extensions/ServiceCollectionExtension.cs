ce87609605b055274132091b86251ac5d28bc879
zaminalili <zaminalili29@gmail.com>
2024-11-04 12:31:50 +0400

    Configure db context and establish connection

--- SkyStorage/SkyStorage.Infrastructure/Extensions/ServiceCollectionExtension.cs
+++ SkyStorage/SkyStorage.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SkyStorage.Infrastructure.Persistence;
 
 namespace SkyStorage.Infrastructure.Extensions;
 
@@ -7,6 +9,10 @@ public static class ServiceCollectionExtension
 {
     public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        
+        string connectionStringName = "SkyStorageLocalDb";
+        var connectionString = configuration.GetConnectionString(connectionStringName);
 
+        services.AddDbContext<SkyStorageDbContext>(options => options.UseSqlServer(connectionString));
     }
 }
---
+++ SkyStorage/SkyStorage.Infrastructure/Persistence/SkyStorageDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SkyStorage.Domain.Entities;
+
+namespace SkyStorage.Infrastructure.Persistence;
+
+internal class SkyStorageDbContext(DbContextOptions<SkyStorageDbContext> options): 
+    IdentityDbContext<User, Role, Guid, 
+        UserClaim, UserRole, UserLogin, RoleClaim, UserToken> (options)
+{
+
+}
