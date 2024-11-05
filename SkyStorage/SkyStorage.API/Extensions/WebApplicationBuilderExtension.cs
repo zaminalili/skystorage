5b26a90e6934e456849cc4716dc7b5e1f9def40e
zaminalili <zaminalili29@gmail.com>
2024-11-05 10:28:43 +0400

    Configure Swagger to support Bearer auth

--- SkyStorage/SkyStorage.API/Extensions/WebApplicationBuilderExtension.cs
+++ SkyStorage/SkyStorage.API/Extensions/WebApplicationBuilderExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace SkyStorage.API.Extensions;
+﻿using Microsoft.OpenApi.Models;
+
+namespace SkyStorage.API.Extensions;
 
 public static class WebApplicationBuilderExtension
 {
@@ -11,6 +13,23 @@ public static class WebApplicationBuilderExtension
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c => {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                       Reference = new OpenApiReference {Type = ReferenceType.SecurityScheme, Id = "bearerAuth"}
+                    },
+                    []
+                }
+            });
+        });
     }
 }
