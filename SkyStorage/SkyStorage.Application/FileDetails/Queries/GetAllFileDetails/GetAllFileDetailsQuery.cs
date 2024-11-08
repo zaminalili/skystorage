0f7b9c0e256a703cfe4d5f65f6a2a3c91ec05abf
zaminalili <zaminalili29@gmail.com>
2024-11-08 09:17:42 +0400

    Create query to retrieve all FileDetails

---
+++ SkyStorage/SkyStorage.Application/FileDetails/Queries/GetAllFileDetails/GetAllFileDetailsQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using SkyStorage.Application.FileDetails.Dtos;
+using SkyStorage.Application.Pagination;
+
+namespace SkyStorage.Application.FileDetails.Queries.GetAllFileDetails;
+
+public class GetAllFileDetailsQuery: IRequest<PagedResult<FileDetailDto>>
+{
+    public Guid UserId { get; set; }
+    public string? searchPhrase { get; set; }
+    public int pageSize { get; set; } = 10;
+    public int pageNumber { get; set; } = 1;
+}
---
+++ SkyStorage/SkyStorage.Application/FileDetails/Queries/GetAllFileDetails/GetAllFileDetailsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using MediatR;
+using SkyStorage.Application.FileDetails.Dtos;
+using SkyStorage.Application.Pagination;
+using SkyStorage.Application.Users;
+using SkyStorage.Domain.Exceptions;
+using SkyStorage.Domain.Repositories;
+
+namespace SkyStorage.Application.FileDetails.Queries.GetAllFileDetails;
+
+public class GetAllFileDetailsQueryHandler(IFileDetailRepository fileDetailRepository, IMapper mapper, IUserContext userContext) : IRequestHandler<GetAllFileDetailsQuery, PagedResult<FileDetailDto>>
+{
+    public async Task<PagedResult<FileDetailDto>> Handle(GetAllFileDetailsQuery request, CancellationToken cancellationToken)
+    {
+        if (request.UserId.ToString() != userContext.GetCurrentUser()!.Id)
+            throw new MismatchException("User", "Current user");
+
+        var (fileDetails, totalCount) = await fileDetailRepository.GetAllAsync(request.UserId, request.searchPhrase, request.pageSize, request.pageNumber);
+
+        var fileDetailDtos = mapper.Map<IEnumerable<FileDetailDto>>(fileDetails);
+        var result = new PagedResult<FileDetailDto>(fileDetailDtos, totalCount, request.pageSize, request.pageNumber);
+
+        return result;
+    }
+}
