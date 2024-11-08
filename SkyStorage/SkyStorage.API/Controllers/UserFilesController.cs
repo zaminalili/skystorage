df2b2c92d50edea65f6b3e88ffba34aa926f2b87
zaminalili <zaminalili29@gmail.com>
2024-11-08 13:49:38 +0400

    Add GetAllFileDetails endpoint

---
+++ SkyStorage/SkyStorage.API/Controllers/UserFilesController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkyStorage.Application.FileDetails.Queries.GetAllFileDetails;
+using SkyStorage.Application.Users;
+
+namespace SkyStorage.API.Controllers
+{
+    [Route("api/users/{userId}/files")]
+    [ApiController]
+    [Authorize]
+    public class UserFilesController(IMediator mediator, ICurrentUserValidator userValidator) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAllFileDetails([FromRoute] Guid userId, [FromQuery] GetAllFileDetailsQuery query)
+        {
+            if(userValidator.IsCurrentUser(userId))
+            {
+                var fileDetails = await mediator.Send(query);
+                return Ok(fileDetails);
+            }
+
+            return BadRequest();
+        }
+    }
+}
