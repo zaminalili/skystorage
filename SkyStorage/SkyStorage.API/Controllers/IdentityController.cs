e93c5b8cf79e9c58db89bad938ab62f5c9a63beb
zaminalili <zaminalili29@gmail.com>
2024-11-08 14:58:49 +0400

    Add GetCurrentUser endpoint

---
+++ SkyStorage/SkyStorage.API/Controllers/IdentityController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkyStorage.Application.Users.Queries.GetCurrentUserId;
+
+namespace SkyStorage.API.Controllers
+{
+    [Route("api/identity")]
+    [ApiController]
+    public class IdentityController(IMediator mediator) : ControllerBase
+    {
+        [HttpGet("currentUser")]
+        [Authorize]
+        public async Task<IActionResult> GetCurrentUser()
+        {
+            var currentUser = await mediator.Send(new GetCurrentUserIdQuery());
+
+            return Ok(currentUser);
+        }
+    }
+}
