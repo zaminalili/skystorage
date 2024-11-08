bc0e546fcc5131064955f5dfcd8d1a8539b7868b
zaminalili <zaminalili29@gmail.com>
2024-11-08 14:49:10 +0400

    Create validation for GetAllFileDetailsQuery

---
+++ SkyStorage/SkyStorage.Application/FileDetails/Queries/GetAllFileDetails/GetAllFileDetailsQueryValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace SkyStorage.Application.FileDetails.Queries.GetAllFileDetails;
+
+public class GetAllFileDetailsQueryValidator: AbstractValidator<GetAllFileDetailsQuery>
+{
+    private int[] allowPageSizes = [5, 10, 15, 20, 25];
+    public GetAllFileDetailsQueryValidator()
+    {
+        RuleFor(b => b.pageNumber).GreaterThanOrEqualTo(1);
+
+        RuleFor(b => b.pageSize)
+            .Must(v => allowPageSizes.Contains(v))
+            .WithMessage($"Page size must be in {string.Join(",", allowPageSizes)}");
+    }
+}
