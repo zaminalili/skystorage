1e122f31373c68e6da8fc557a753b1e3b430c1eb
zaminalili <zaminalili29@gmail.com>
2024-11-13 18:41:57 +0400

    Add tests for validators

---
+++ Tests/SkyStorage.Application.Tests/FileDetails/Commands/UploadFile/FormFileValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation;
+using FluentValidation.TestHelper;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.ComponentModel.DataAnnotations;
+using Xunit;
+
+namespace SkyStorage.Application.FileDetails.Commands.UploadFile.Tests;
+
+public class FormFileValidatorTests
+{
+    private readonly FormFileValidator validator;
+
+    public FormFileValidatorTests()
+    {
+        this.validator = new FormFileValidator();
+    }
+
+    private IFormFile CreateMockFormFile(long sizeInBytes)
+    {
+        var fileMock = new Mock<IFormFile>();
+        fileMock.Setup(f => f.Length).Returns(sizeInBytes);
+        return fileMock.Object;
+    }
+
+
+    [Fact()]
+    public void Should_HaveError_WhenFileExceedsMaxSize()
+    {
+        // Arrange
+        var oversizedFile = CreateMockFormFile(11 * 1024 * 1024); // 11 MB
+
+        // Act
+        var result = validator.TestValidate(oversizedFile);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(file => file)
+              .WithErrorMessage("The maximum file size is 10 MB.");
+    }
+
+    [Fact()]
+    public void Should_NotHaveError_WhenFileIsValid()
+    {
+        // Arrange
+        var validFile = CreateMockFormFile(5 * 1024 * 1024); // 5 MB
+
+        // Act
+        var result = validator.TestValidate(validFile);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(file => file);
+    }
+}
---
+++ Tests/SkyStorage.Application.Tests/FileDetails/Queries/GetAllFileDetails/GetAllFileDetailsQueryValidatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using FluentValidation.TestHelper;
+
+namespace SkyStorage.Application.FileDetails.Queries.GetAllFileDetails.Tests;
+
+public class GetAllFileDetailsQueryValidatorTests
+{
+    private readonly GetAllFileDetailsQueryValidator validator;
+    public GetAllFileDetailsQueryValidatorTests()
+    {
+        this.validator = new GetAllFileDetailsQueryValidator();
+    }
+
+    [Fact()]
+    public void Should_NotHaveError_ForValidQuery()
+    {
+        // arrange
+        var query = new GetAllFileDetailsQuery()
+        {
+            searchPhrase = "",
+            pageNumber = 1,
+            pageSize = 10,
+        };
+
+        // act
+        var result = validator.TestValidate(query);
+
+        // asset
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Fact()]
+    public void Should_HaveError_ForInvalidQuery()
+    {
+        // arrange
+        var query = new GetAllFileDetailsQuery()
+        {
+            searchPhrase = "",
+            pageNumber = 2,
+            pageSize = 2,
+        };
+
+        // act
+        var result = validator.TestValidate(query);
+
+        // asset
+        result.ShouldHaveAnyValidationError();
+    }
+}
